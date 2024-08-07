2d738f0b79066c529ef7315886c748417af63e5b
Tathagata Banerjee <42165299+tatsbans@users.noreply.github.com>
2024-08-07 09:03:32 +0530

    10th  commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent {
   users = DUMMY_USERS;
   selectedUserId?: string;
   randCount!:string;
-
+  ace:Number[] = [1,2,3,4,5]
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId);
   }
